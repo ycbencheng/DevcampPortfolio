151e893cb05df50fe6f121f0788d3dd481c5aba7
ycbencheng <bcyc2004@gmail.com>
2017-02-24 16:40:55 -0700

    implemened destroy funct

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,9 +39,19 @@ class PortfoliosController < ApplicationController
     end
 
   end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'It was successfully destroyed.' }  
+  end
+end
+end
 
