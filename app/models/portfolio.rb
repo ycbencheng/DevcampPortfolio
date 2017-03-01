db93f4fa67d8a91da9fbf7604f2fbfa0af0d75ff
ycbencheng <bcyc2004@gmail.com>
2017-03-01 09:03:39 -0700

    Add concern for managing managing image data
    exit
    exit

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -11,7 +12,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://placehold.it/350x350"
-    self.thumb_image = "http://placehold.it/150x150"
+    self.main_image ||= Placeholder.image_generator(height: '350', width: '250')
+    self.thumb_image ||= Placeholder.image_generator(height: '150', width: '250')
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,8 @@
 class Skill < ApplicationRecord
-
-validates_presence_of :title, :percent_usage
+  include Placeholder
+  validates_presence_of :title, :percent_usage
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+    
+  end
 end
