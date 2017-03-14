2c00c763a0c8fe10fff6de9c92c78946a7220afa
ycbencheng <bcyc2004@gmail.com>
2017-03-14 16:55:41 -0600

    Updated files upload form refactor placeholder functionality into view

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,3 @@
 class Skill < ApplicationRecord
-
-  include Placeholder
   validates_presence_of :title, :percent_usage
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-    
-  end
 end
