3fc129ebada63a0704b2c6a77337d18e1f971567
ycbencheng <bcyc2004@gmail.com>
2017-03-21 10:19:28 -0600

    Add ruby

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -5,7 +5,7 @@ module BlogsHelper
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.scan(code, language ||= :ruby).div
     end
   end
 
